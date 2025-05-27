f112d89d848410d73de26e41b5baaa854c97c6a4
pupue <reika02955@gmail.com>
2025-05-27 11:57:14 +0900

    リストUI作成

---
+++ src/app/_components/MemoBody.tsx
@@ -0,0 +1,126 @@
+"use client";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Trash2 as DeleteIcon, Check as DoneIcon, SquarePen as EditIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { codeBlock } from "@blocknote/code-block";
+import { BlockNoteSchema, defaultBlockSpecs } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import Markdown from "react-markdown";
+
+type Props = {
+	title: string;
+	contents: string;
+};
+
+const schema = BlockNoteSchema.create({
+	blockSpecs: {
+		paragraph: defaultBlockSpecs.paragraph,
+		heading: defaultBlockSpecs.heading,
+		bulletListItem: defaultBlockSpecs.bulletListItem,
+		codeBlock: defaultBlockSpecs.codeBlock,
+	},
+});
+
+export default function MemoBody({ title, contents }: Props) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [markdown, setMarkdown] = useState<string>(contents);
+
+	const editor = useCreateBlockNote({ schema, codeBlock });
+
+	useEffect(() => {
+		async function loadInitialHTML() {
+			const blocks = await editor.tryParseMarkdownToBlocks(contents);
+			editor.replaceBlocks(editor.document, blocks);
+		}
+		loadInitialHTML();
+	}, [editor, contents]);
+
+	const handeSave = async () => {
+		if (!editor) return;
+		const blocks = editor.document;
+		const markdownFromBlocks = await editor.blocksToMarkdownLossy(blocks);
+		setMarkdown(markdownFromBlocks);
+		setIsEditing(false);
+	};
+
+	if (!editor) return <p>読み込み中...</p>;
+
+	return (
+		<div>
+			{isEditing ? (
+				<BlockNoteView editor={editor} />
+			) : (
+				<div className="bn-default-styles">
+					<Markdown
+						components={{
+							h1: () => null,
+							h2: ({ node, children, ...props }) => {
+								const id = crypto.randomUUID();
+								return (
+									<div className="bn-block-outer" data-node-type="blockOuter" data-id={id}>
+										<div className="bn-block" data-node-type="blockContainer" data-id={id}>
+											<div className="bn-block-content" data-content-type="heading" data-level="2">
+												<h2 className="bn-inline-content" {...props}>
+													{children}
+												</h2>
+											</div>
+										</div>
+									</div>
+								);
+							},
+						}}
+					>
+						{markdown}
+					</Markdown>
+				</div>
+			)}
+
+			<div className="flex justify-end space-x-2 mt-2">
+				{isEditing ? (
+					<Button onClick={handeSave} className="bg-green-500 hover:bg-green-500/90 text-white">
+						<DoneIcon />
+					</Button>
+				) : (
+					<Button variant="outline" onClick={() => setIsEditing(true)}>
+						<EditIcon />
+					</Button>
+				)}
+
+				<AlertDialog>
+					<AlertDialogTrigger asChild>
+						<Button variant="destructive">
+							<DeleteIcon />
+						</Button>
+					</AlertDialogTrigger>
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+							<AlertDialogDescription>
+								This action cannot be undone. This will permanently delete your account and remove your data from our
+								servers.
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogCancel>Cancel</AlertDialogCancel>
+							<AlertDialogAction>Continue</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
+			</div>
+		</div>
+	);
+}
